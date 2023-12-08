5ecff5835cb2eef2eb5e1f3ff09e2543648abc30
Hisham Salam <120439936+hisham-slm@users.noreply.github.com>
2023-12-08 10:47:46 +0530

    Update settings.py

--- personal_management/settings.py
+++ personal_management/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'management',
         'USER': 'postgres',
-        'PASSWORD' : 'blahcoat',
+        'PASSWORD' : '',
         'HOST': 'localhost',
         'PORT':'5432'
     }
